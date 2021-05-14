f07e152be2aaf88e365b4384a7208087efa7aaf7
WexCore <wexcore@vivaldi.net>
2021-05-14 19:21:46 +0300

    Fix theme changing

--- themes/hello-friend-ng/assets/js/main.js
+++ themes/hello-friend-ng/assets/js/main.js
@@ -10,17 +10,18 @@ const chosenTheme = window.localStorage && window.localStorage.getItem("theme");
 const chosenThemeIsDark = chosenTheme == "dark";
 const chosenThemeIsLight = chosenTheme == "light";
 
+if (chosenTheme == null){ window.localStorage.setItem("theme", "light"); }
 // Detect the color scheme the operating system prefers.
 function detectOSColorTheme() {
   if (chosenThemeIsDark) {
     document.documentElement.setAttribute("data-theme", "dark");
-  } else if (chosenThemeIsLight) {
+  } else  {
     document.documentElement.setAttribute("data-theme", "light");
-  } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    document.documentElement.setAttribute("data-theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-  }
+  } //else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    //document.documentElement.setAttribute("data-theme", "dark");
+  //} else {
+    //document.documentElement.setAttribute("data-theme", "light");
+  //}
 }
 
 // Switch the theme.
@@ -36,17 +37,17 @@ function switchTheme(e) {
 }
 
 // Event listener
+
 if (themeToggle) {
   themeToggle.addEventListener("click", switchTheme, false);
-  window
-    .matchMedia("(prefers-color-scheme: dark)")
-    .addEventListener("change", (e) => e.matches && detectOSColorTheme());
-  window
-    .matchMedia("(prefers-color-scheme: light)")
-    .addEventListener("change", (e) => e.matches && detectOSColorTheme());
+  // window
+  //   .matchMedia("(prefers-color-scheme: dark)")
+  //   .addEventListener("change", (e) => e.matches && detectOSColorTheme());
+  // window
+  //   .matchMedia("(prefers-color-scheme: light)")
+  //   .addEventListener("change", (e) => e.matches && detectOSColorTheme());
 
   detectOSColorTheme();
 } else {
   localStorage.removeItem("theme");
 }
-
